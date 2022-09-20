c74f8dd0716c4a8aafd8154c3996d4b37354ba18
yzqdev <yzqdev@outlook.com>
2022-09-20 08:47:45 +0800

    update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     path: 'help', component: HelpPageComponent
   },
   {
-    path: "", component: AppComponent
+    path: "", component: AppComponent,
   },
   {
     path: 'hero', component: HeroesComponent
